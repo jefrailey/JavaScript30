d516b081bf2d5e535819cbe29fdfd2325c93b732
jefrailey <jcaetof@gmail.com>
2017-01-16 12:51:43 -0800

    Pre-video attempt at 11 - Custom Video Player

---
+++ 11 - Custom Video Player/my-scripts.js
@@ -0,0 +1,35 @@
+const video = document.querySelector('video');
+const playerControls = document.querySelector('div.player__controls');
+const playButton = playerControls.querySelector('.player__button.toggle');
+const volume = playerControls.querySelector('input[name="volume"]');
+const playbackRate = playerControls.querySelector('input[name="playbackRate"]');
+const skips = playerControls.querySelectorAll('button[data-skip]');
+const totalProgress = playerControls.querySelector('.progress');
+const filled = playerControls.querySelector('.progress__filled');
+filled.style.flexBasis = '0%';
+function togglePlay() {
+    if (video.paused) {video.play();}
+    else {video.pause();}
+}
+function changeRate() {
+    video.playbackRate = this.value;
+}
+function changeVolume() {
+    video.volume = this.value;
+}
+function skip() {
+    video.currentTime = video.currentTime + Number(this.dataset.skip);
+}
+function updateProgress() {
+    filled.style.flexBasis = `${(video.currentTime / video.duration) * 100}%`;
+}
+function jumpToPercent(event) {
+    video.currentTime = (event.offsetX / video.clientWidth) * video.duration;
+}
+video.addEventListener('click', togglePlay);
+video.addEventListener('timeupdate', updateProgress);
+playButton.addEventListener('click', togglePlay);
+playbackRate.addEventListener('change', changeRate);
+volume.addEventListener('change', changeVolume);
+skips.forEach(button => button.addEventListener('click', skip));
+totalProgress.addEventListener('click', jumpToPercent);
