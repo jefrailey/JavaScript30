b0fa6fdaff3751ad070507c6371287af870c7680
jefrailey <jcaetof@gmail.com>
2017-03-30 20:57:41 -0700

    Start 19 Webcam Fun with getVideo; paintToCanvas

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,25 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+  navigator.mediaDevices.getUserMedia(
+    {video: true, audio: false}
+  )
+  .then(localMediaStream => {
+    video.src = window.URL.createObjectURL(localMediaStream);
+    video.play();
+  })
+  .catch(err => console.error('Danger Will', err));
+}
+
+function paintToCanvas() {
+  const {videoWidth: width, videoHeight: height} = video;
+  console.log(width, height);
+  canvas.width = width;
+  canvas.height = height;
+
+  setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+  }, 16);
+}
